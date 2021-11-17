8f8149321562541fa92bf15dc9a9a88da19cb101
Matthew Francis <mattfrancis888@gmail.com>
2021-11-17 16:05:58 -0500

    Set up backend.

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+const app = express();
+// middleware for parsing bodies from URL
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+
+console.log("NODE ENV", process.env.privateKey);
+app.use("/api/test", (req, res) => {
+    res.send("hi");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
